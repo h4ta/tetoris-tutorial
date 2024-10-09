0b236b2f46edea83f603d72981b956687c92ac95
Hata <141723500+h4ta@users.noreply.github.com>
2024-10-09 02:31:44 +0000

    moveLeft, moveRightとrotateLeft, rotateRigjtをそれぞれmoveHorizontal, rotateMinoにまとめた

--- src/component/Controller.tsx
+++ src/component/Controller.tsx
@@ -1,6 +1,5 @@
 import { Dispatch, SetStateAction, useEffect, useRef } from "react";
 import { MinoCell } from "./Mino";
-import { useBeginningField } from "../hooks/useBeginningField";
 
 type Props = {
   field: Array<Array<MinoCell>>;
@@ -45,13 +44,12 @@ export const Controller = (props: Props) => {
     nextMino,
   } = props;
 
-  const { beginningField } = useBeginningField();
-
   function sleep(ms: number): Promise<void> {
     return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
-  const moveRight = () => {
+  // 動かす向き(左: 負、右: 正)、動くマスの数(絶対値)を受け取り、動かす関数
+  const moveHorizontal = (moveVec: number) => {
     let nextField: Array<Array<MinoCell>> = [...field];
 
     const moveField: Array<[number, number, MinoCell]> = [];
@@ -70,68 +68,25 @@ export const Controller = (props: Props) => {
       });
     });
 
-    // 右にミノを移動できるか確認
-    if (checkMovable(moveCells, 1, 0)) {
-      // 出来たら右に移動
+    // 指定された方向にミノを移動できるか確認
+    if (checkMovable(moveCells, moveVec, 0)) {
+      // 出来たら指定された方向に移動
       moveField.forEach(([i, j, nowCell]) => {
         // 壁と重ならないセルのみ動かす
-        if (!nextField[i][j + 1].isFinished) {
-          nextField[i][j + 1] = nowCell;
-        }
-      });
-
-      // fieldを戻す処理
-      if (moveField.length !== 16) {
-        for (let i = 0; i < 4; i++) {
-          const nextCell = moveField[3 * i];
-          nextField[nextCell[0]][nextCell[1]] = minoField;
-        }
-      }
-    } else {
-      // 出来なかったら、
-      // なくなっている箇所を戻す
-      moveField.forEach(([i, j, nowCell]) => {
-        nextField[i][j] = nowCell;
-      });
-    }
-
-    setField(nextField);
-
-    console.log(moveField);
-  };
-  const moveLeft = () => {
-    let nextField: Array<Array<MinoCell>> = [...field];
-
-    const moveField: Array<[number, number, MinoCell]> = [];
-    const moveCells: Array<[number, number, MinoCell]> = []; // こっちはミノの部分だけ
-
-    nextField.forEach((row, i) => {
-      row.forEach((cell, j) => {
-        if (!cell.isFinished && cell.minoType) {
-          nextField[i][j] = space;
-          const nowCell = { ...cell };
-          moveField.push([i, j, nowCell]);
-          if (nowCell.minoType !== 9) {
-            moveCells.push([i, j, nowCell]);
-          }
-        }
-      });
-    });
-
-    // 左にミノを移動できるか確認
-    if (checkMovable(moveCells, -1, 0)) {
-      // 出来たら左に移動
-      moveField.forEach(([i, j, nowCell]) => {
-        // 壁と重ならないセルのみ動かす
-        if (!nextField[i][j - 1].isFinished) {
-          nextField[i][j - 1] = nowCell;
+        if (!nextField[i][j + moveVec].isFinished) {
+          nextField[i][j + moveVec] = nowCell;
         }
       });
 
       // fieldを戻す処理
       if (moveField.length === 12) {
+        let nextCell;
         for (let i = 0; i < 4; i++) {
-          const nextCell = moveField[3 * i + 2];
+          if (moveVec) {
+            nextCell = moveField[3 * i];
+          } else {
+            nextCell = moveField[3 * i + 2];
+          }
           nextField[nextCell[0]][nextCell[1]] = minoField;
         }
       }
@@ -270,70 +225,8 @@ export const Controller = (props: Props) => {
     }
   };
 
-  const rotateRight = () => {
-    let nextField: Array<Array<MinoCell>> = [...field];
-
-    const moveSquares: Array<[number, number, MinoCell]> = [];
-
-    nextField.forEach((row, i) => {
-      row.forEach((cell, j) => {
-        // 現在動かしているspaceでないブロックを取得
-        if (!cell.isFinished && cell.minoType) {
-          console.log(cell);
-
-          nextField[i][j] = space;
-          const nowCell = { ...cell };
-          moveSquares.push([i, j, nowCell]);
-        }
-      });
-    });
-
-    console.log(moveSquares);
-
-    // fieldに他ブロックが重なっているとき、処理を終了
-    if (moveSquares.length !== 16) {
-      // なくなっている箇所を戻す
-      moveSquares.forEach(([i, j, nowCell]) => {
-        nextField[i][j] = nowCell;
-      });
-      return;
-    }
-
-    // 現在動かすミノを取得
-    let rotateField: Array<Array<MinoCell>> = [
-      [space, space, space, space],
-      [space, space, space, space],
-      [space, space, space, space],
-      [space, space, space, space],
-    ];
-
-    const corner = moveSquares[0];
-    moveSquares.forEach((squares, i) => {
-      rotateField[Math.floor(i / 4)][i % 4] = squares[2];
-    });
-
-    console.log(moveSquares);
-    console.log(rotateField);
-
-    // 右90度回転
-    const resultField = rotateField.map((row, i) => {
-      return row.map((square, j) => {
-        return rotateField[rotateField.length - 1 - j][i];
-      });
-    });
-
-    // フィールドに描画;
-    resultField.forEach((row, i) => {
-      row.forEach((square, j) => {
-        nextField[corner[0] + i][corner[1] + j] = square;
-      });
-    });
-
-    setField(nextField);
-    console.log("終了");
-  };
-
-  const rotateLeft = () => {
+  // 回転の向きを受け取り、その方向にミノを回転させる関数
+  const rotateMino = (direction: "left" | "right") => {
     let nextField: Array<Array<MinoCell>> = [...field];
 
     const moveSquares: Array<[number, number, MinoCell]> = [];
@@ -376,12 +269,23 @@ export const Controller = (props: Props) => {
     console.log(moveSquares);
     console.log(rotateField);
 
-    // 左90度回転
-    const resultField = rotateField.map((row, i) => {
-      return row.map((square, j) => {
-        return rotateField[j][rotateField.length - 1 - i];
+    // 指定された向きに90度回転
+    let resultField: Array<Array<MinoCell>>;
+    if (direction === "left") {
+      // 左向きの場合
+      resultField = rotateField.map((row, i) => {
+        return row.map((square, j) => {
+          return rotateField[j][rotateField.length - 1 - i];
+        });
       });
-    });
+    } else {
+      // 右向きの場合
+      resultField = rotateField.map((row, i) => {
+        return row.map((square, j) => {
+          return rotateField[rotateField.length - 1 - j][i];
+        });
+      });
+    }
 
     // フィールドに描画;
     resultField.forEach((row, i) => {
@@ -393,7 +297,7 @@ export const Controller = (props: Props) => {
     setField(nextField);
   };
 
-  // 動かすミノの場所、動かしたい方向を受け取る
+  // 動かすミノの場所、動かしたい方向を受け取る関数
   const checkMovable = (
     nowArea: Array<[number, number, MinoCell]>,
     x: number,
@@ -478,14 +382,6 @@ export const Controller = (props: Props) => {
     }
   }, [isGameOver]);
 
-  const restart = () => {
-    setField(beginningField);
-    setMinoCount((n) => n + 1);
-    setIsGameOver(false);
-    setIsPlayingNow(true);
-    startAutoFall();
-  };
-
   return (
     <>
       <div className="buttonContainer">
@@ -496,15 +392,15 @@ export const Controller = (props: Props) => {
         </div>
         <div>
           <div>
-            <button onClick={moveLeft}>⬅</button>
+            <button onClick={() => moveHorizontal(-1)}>⬅</button>
             <button onClick={moveDown}>⬇</button>
-            <button onClick={moveRight}>⮕</button>
+            <button onClick={() => moveHorizontal(1)}>⮕</button>
           </div>
         </div>
         <div>
           <div>
-            <button onClick={rotateRight}>⟳</button>
-            <button onClick={rotateLeft}>⟲</button>
+            <button onClick={() => rotateMino("right")}>⟳</button>
+            <button onClick={() => rotateMino("left")}>⟲</button>
           </div>
         </div>
         <div>
